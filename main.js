19fcb1f9078c4676cf4d948ff596696062bd34f2
Dr3wbit <eightbitandrew@gmail.com>
2018-03-26 11:01:38 -0700

    added Ailment text on left side

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ ailmentSound.src = 'soundFX/ailment.wav';
 
 var winSound = new Audio();
 winSound.src = 'soundFX/win_sound.wav';
-winSound.volume = .5;
+winSound.volume = .2;
 
 var cardTypeArray = ['exhaustion','exhaustion','dysentery','dysentery','typhoid','typhoid','measles','measles','freshWater','freshWater','heartyFood','heartyFood','restStop','restStop','oxen','oxen','river','river','tree','tree','rifle','rifle','cactus','cactus','bovineSkull','bovineSkull','deer','deer','boulder','boulder'];
 var cardMemory = null;
@@ -72,10 +72,12 @@ function dealCards() {
 function revealCardFace(clickInput) {
     var clickedCard = $(clickInput);
     var cardType = $(clickInput).attr('type');
+    var Ailment = null;
     cardsCurrentlyFlipped++;
     clickedCard.addClass(cardType).removeClass('card');
     cardClickSound.play();
     var time = $('.timerBar');
+    var i = 0;
 
     if (cardMemory === null && cardsCurrentlyFlipped === 1) {
         cardMemory = [];
@@ -113,53 +115,65 @@ function revealCardFace(clickInput) {
             $("[type='" + cardMemory[1] + "']").addClass('disableClick');
 
             if (cardMemory[0] === "exhaustion" && cardMemory[1] === "exhaustion"){
+                Ailment = $('<div class="exhaustionText">');
+                $('#currentAilments').append(Ailment);
+                Ailment.text('Exhausted');
                 $('#mainText').text('You Are Exhausted...');
                 ailmentSound.play();
-                for(i=0;i<10;i++) {
+                for(i=0;i<15;i++) {
                     (function(i) {
                         setTimeout(function () {
                             timerBarDepletionCounter++;
                             $(time[timerBarDepletionCounter]).addClass('depleted');
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
 
             else if(cardMemory[0]  === "dysentery" && cardMemory[1] === "dysentery"){
+                Ailment = $('<div class="dysenteryText">');
+                $('#currentAilments').append(Ailment);
+                Ailment.text('Dysentery');
                 $('#mainText').text('You Have Dysentery...');
                 ailmentSound.play();
-                for(i=0;i<10;i++) {
+                for(i=0;i<15;i++) {
                     (function(i) {
                         setTimeout(function () {
                             timerBarDepletionCounter++;
                             $(time[timerBarDepletionCounter]).addClass('depleted');
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
 
             else if(cardMemory[0] === "typhoid" && cardMemory[1] === "typhoid"){
+                Ailment = $('<div class="typhoidText">');
+                $('#currentAilments').append(Ailment);
+                Ailment.text('Typhoid');
                 $('#mainText').text('You Have Typhoid...');
                 ailmentSound.play();
-                for(i=0;i<10;i++) {
+                for(i=0;i<15;i++) {
                     (function(i) {
                         setTimeout(function () {
                             timerBarDepletionCounter++;
                             $(time[timerBarDepletionCounter]).addClass('depleted');
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
 
             else if(cardMemory[0] === "measles" && cardMemory[1] === "measles"){
+                Ailment = $('<div class="measlesText">');
+                $('#currentAilments').append(Ailment);
+                Ailment.text('Measles');
                 $('#mainText').text('You Have Measles...');
                 ailmentSound.play();
-                for(i=0;i<10;i++) {
+                for(i=0;i<15;i++) {
                     (function(i) {
                         setTimeout(function () {
                             timerBarDepletionCounter++;
                             $(time[timerBarDepletionCounter]).addClass('depleted');
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
@@ -167,12 +181,12 @@ function revealCardFace(clickInput) {
             else if(cardMemory[0] === "freshWater" && cardMemory[1] === "freshWater"){
                 $('#mainText').text('You Feel Hydrated!');
                 cardMatchSound.play();
-                for(i=0;i<6;i++) {
+                for(i=0;i<8;i++) {
                     (function (i) {
                         setTimeout(function () {
                             $(time[timerBarDepletionCounter]).removeClass('depleted');
                             timerBarDepletionCounter--;
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
@@ -180,12 +194,12 @@ function revealCardFace(clickInput) {
             else if(cardMemory[0] === "heartyFood" && cardMemory[1] === "heartyFood"){
                 $('#mainText').text('You Feel Full!');
                 cardMatchSound.play();
-                for(i=0;i<6;i++) {
+                for(i=0;i<8;i++) {
                     (function (i) {
                         setTimeout(function () {
                             $(time[timerBarDepletionCounter]).removeClass('depleted');
                             timerBarDepletionCounter--;
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
@@ -193,12 +207,12 @@ function revealCardFace(clickInput) {
             else if(cardMemory[0] === "restStop" && cardMemory[1] === "restStop"){
                 $('#mainText').text('You Feel Rested!');
                 cardMatchSound.play();
-                for(i=0;i<6;i++) {
+                for(i=0;i<8;i++) {
                     (function (i) {
                         setTimeout(function () {
                             $(time[timerBarDepletionCounter]).removeClass('depleted');
                             timerBarDepletionCounter--;
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
@@ -206,12 +220,12 @@ function revealCardFace(clickInput) {
             else if(cardMemory[0] === "rifle" && cardMemory[1] === "rifle"){
                 $('#mainText').text('You Gain Food From Your Hunt!');
                 cardMatchSound.play();
-                for(i=0;i<6;i++) {
+                for(i=0;i<8;i++) {
                     (function (i) {
                         setTimeout(function () {
                             $(time[timerBarDepletionCounter]).removeClass('depleted');
                             timerBarDepletionCounter--;
-                        }, 500*i);
+                        }, 300*i);
                     })(i);
                 }
             }
@@ -249,6 +263,7 @@ function winGame(){
 function resetGame(){
     $('#gameBody').empty();
     $('#rightSideBar').empty();
+    $('#currentAilments').empty();
     cardsCurrentlyFlipped = 0;
     cardMemory = null;
     timerBarDepletionCounter = -1;
