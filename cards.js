5ee57e7dc9cfbeb7dd892d114f964d01c2b8b3ff
Andrew Harrington <eightbitandrew@gmail.com>
2018-08-31 21:18:41 -0700

    converting cards to OOP

--- cards.js
+++ cards.js
@@ -1,16 +1,212 @@
 const cardTypes = {
 
+    exhaustion: {
+        name: 'exhaustion',
+        count: 2,
+        image: 'exhaustion.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    dysentery: {
+        name: 'dysentery',
+        count: 2,
+        image: 'dysentery.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    typhoid: {
+        name: 'typhoid',
+        count: 2,
+        image: 'typhoid.png',
+        onFirstclick: function () {
+            displayEffect();
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    measles: {
+        name: 'measles',
+        count: 2,
+        image: 'measles.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    freshWater: {
+        name: 'freshWater',
+        count: 2,
+        image: 'freshWater.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    heartyFood: {
+        name: 'heartyFood',
+        count: 2,
+        image: 'heartyFood.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
     restStop: {
         name: 'restStop',
         count: 2,
         image: 'restStop.png',
         onFirstclick: function () {
-            displayEffect(chooseRandom(['This looks promising', 'this might be a safe place...', 'a nap here couldn\'t hurt, right?']));
+            displayEffect( );
         },
         onMatch: function () {
-            shiftLifeIndicator(5);
+            shiftHealthIndicator();
             playMatchSound();
-            displayEffect('You Feel Rested!');
+            displayEffect('');
+        },
+    },
+    
+    oxen: {
+        name: 'oxen',
+        count: 2,
+        image: 'oxen.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    river: {
+        name: 'river',
+        count: 2,
+        image: 'river.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    tree: {
+        name: 'tree',
+        count: 2,
+        image: 'tree.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    rifle: {
+        name: 'rifle',
+        count: 2,
+        image: 'rifle.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    cactus: {
+        name: 'cactus',
+        count: 2,
+        image: 'cactus.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    bovineSkull: {
+        name: 'bovineSkull',
+        count: 2,
+        image: 'bovineSkull.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    deer: {
+        name: 'deer',
+        count: 2,
+        image: 'deer.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
+        },
+    },
+    
+    boulder: {
+        name: 'boulder',
+        count: 2,
+        image: 'boulder.png',
+        onFirstclick: function () {
+            displayEffect( );
+        },
+        onMatch: function () {
+            shiftHealthIndicator();
+            playMatchSound();
+            displayEffect('');
         },
     },
 }
