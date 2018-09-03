cf2af8ad8382192a9fe4cfa7e4a48e798c5eddf6
Andrew Harrington <eightbitandrew@gmail.com>
2018-09-03 14:23:23 -0700

    adjusted file directory for webserver

--- main.js
+++ main.js
@@ -372,27 +372,3 @@ function toggleMusic() {
         $('#muteMusicButton').text('Play Music');
     }
 }
-
-const cardClickSound = new Audio();
-cardClickSound.src = 'soundFX/card_clicked.wav';
-cardClickSound.volume = 0.1;
-
-const noMatchSound = new Audio();
-noMatchSound.src = 'soundFX/no_match.wav';
-noMatchSound.volume = .5;
-
-const cardMatchSound = new Audio();
-cardMatchSound.src = 'soundFX/match-made.wav';
-cardMatchSound.volume = .2;
-
-const themeSong = new Audio();
-themeSong.src = 'soundFX/main_song.mp3';
-themeSong.volume = .2;
-themeSong.autoplay = true;
-
-const ailmentSound = new Audio();
-ailmentSound.src = 'soundFX/ailment.wav';
-
-const winSound = new Audio();
-winSound.src = 'soundFX/win_sound.wav';
-winSound.volume = .2;
--- sounds.js
+++ sounds.js
@@ -1,25 +1,25 @@
-var cardClickSound = new Audio();
+const cardClickSound = new Audio();
 cardClickSound.src = 'soundFX/card_clicked.wav';
 cardClickSound.volume = 0.1;
 
-var noMatchSound = new Audio();
+const noMatchSound = new Audio();
 noMatchSound.src = 'soundFX/no_match.wav';
 noMatchSound.volume = .5;
 
-var cardMatchSound = new Audio();
+const cardMatchSound = new Audio();
 cardMatchSound.src = 'soundFX/match-made.wav';
 cardMatchSound.volume = .2;
 
-var themeSong = new Audio();
+const themeSong = new Audio();
 themeSong.src = 'soundFX/main_song.mp3';
 themeSong.volume = .2;
 themeSong.onpause = function(){
     this.play();
 };
 
-var ailmentSound = new Audio();
+const ailmentSound = new Audio();
 ailmentSound.src = 'soundFX/ailment.wav';
 
-var winSound = new Audio();
+const winSound = new Audio();
 winSound.src = 'soundFX/win_sound.wav';
 winSound.volume = .2;
